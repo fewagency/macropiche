235fe226436d2932c23024699c95ab9f109ce0ca
bjuppa <bjorn.nilsved@fewagency.se>
2017-05-13 18:33:37 +0200

    Include instead of require

--- examples/test.php
+++ examples/test.php
@@ -1,6 +1,6 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/support/test-blade-loader.php'
+include __DIR__ . '/support/test-blade-loader.php'
 ?>
 <!DOCTYPE html>
 <html>
