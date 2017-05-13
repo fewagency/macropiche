df355f48f9388cc0063762589249592e6f57db32
bjuppa <bjorn.nilsved@fewagency.se>
2017-05-13 19:13:48 +0200

    Add html file extension

--- examples/test-blade-jenssegers.php
+++ examples/test-blade-jenssegers.php
@@ -1,10 +1,12 @@
 <?php
 function macropiche_blade_view()
 {
-    return new \Jenssegers\Blade\Blade(
+    $blade = new \Jenssegers\Blade\Blade(
         [realpath(__DIR__)],
         realpath(__DIR__ . '/blade-cache')
     );
+    $blade->getFinder()->addExtension('html');
+    return $blade;
 }
 
 require 'test.php';
